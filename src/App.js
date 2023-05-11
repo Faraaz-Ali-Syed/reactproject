45b655cd3cf8c05492365879863118e399b0d9c8
Syed.Faraaz.Ali <Syed.Faraaz.Ali@skillassure.com>
2023-05-12 01:06:59 +0530

    making more components

--- src/App.js
+++ src/App.js
@@ -14,19 +14,19 @@ function App() {
       id: "e2",
       title: "Car Insurance",
       amount: 924.23,
-      date: new Date(2022, 13, 11),
+      date: new Date(2019, 1, 1),
     },
     {
       id: "e3",
       title: "TV",
       amount: 54.21,
-      date: new Date(2023, 1, 1),
+      date: new Date(2023, 1, 5),
     },
     {
       id: "e4",
       title: "Mobile",
       amount: 94.23,
-      date: new Date(2020, 1, 14),
+      date: new Date(2020, 2, 14),
     },
   ];
 
@@ -36,24 +36,23 @@ function App() {
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
-        date={expenses[0].date} 
-      ></ExpenseItem>
+        date={expenses[0].date}
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
-
+      />
     </div>
   );
 }
