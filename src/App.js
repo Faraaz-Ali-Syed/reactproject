5494010f0317825276f356bfaf082c7fca533a13
Syed.Faraaz.Ali <Syed.Faraaz.Ali@skillassure.com>
2023-05-12 14:13:53 +0530

    Lets get started 12

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
-import Expenses from "./components/Expenses";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
+import React from 'react';
 
 function App() {
-
-  return (
-    <div>
-      <h2> Lets get started </h2>
-      <Expenses items="{expenses}"
-      />
-    </div>
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Let's get started!"),
+    React.createElement(Expenses, {items: Expenses})
   );
+
+  // return (
+  //   <div>
+  //     <h2> Lets get started </h2>
+  //     <Expenses items="{expenses}"
+  //     />
+  //   </div>
 }
 
 export default App;
