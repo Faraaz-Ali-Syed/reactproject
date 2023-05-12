cf13aeafb94c927189889f9f0f1868df00044fd4
Syed.Faraaz.Ali <Syed.Faraaz.Ali@skillassure.com>
2023-05-12 16:13:25 +0530

    State new component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
-    const titleChangesHandler = () => {
-        console.log("Title Changed!");
+    const titleChangesHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    }
+    const amountChangesHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    }
+    const dateChangesHandler = (event) => {
+        setEnteredDate(event.target.value);
     }
     return (<form>
         <div className="new-expense__controls">
